575e0c4deb04d510b15ada0ea9fa470fb1aa6a12
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-11-27 15:41:29 +0000

    initial commit

---
+++ jasmine-standalone-3.3.0/spec/thermostatSpec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+   expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('increases in temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in temperature with down()', function() {
+    thermostat.down();
+  expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+});
---
+++ jasmine-standalone-3.3.0/src/thermostat.js
@@ -0,0 +1,17 @@
+'use strict';
+
+  function Thermostat() {
+    this.temperature = 20;
+  }
+
+  Thermostat.prototype.getCurrentTemperature = function() {
+    return this.temperature;
+  };
+
+  Thermostat.prototype.up = function() {
+    this.temperature += 1;
+  };
+
+  Thermostat.prototype.down = function() {
+  this.temperature -= 1;
+};
